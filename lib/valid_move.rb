36e71ad56c0f157eb5ddd3d1d668a1f53635055d
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-19 13:22:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,10 +3,11 @@
 def valid_move?(board, position)
 	if !position_taken?(board, position) || position.between?(0,8) || board[index]
 		true
-	else position_taken?(board, spot) || !spot.between?(0,8) || !board[index]
+	else position_taken?(board, spot) || !position.between?(0,8) || !board[index]
 	  false
 	end
 end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, index)
