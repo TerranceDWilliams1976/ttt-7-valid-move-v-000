01f2fa3a04984cce041bf119d0750a6175c0bab7
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-19 13:38:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -23,4 +23,12 @@ def position_taken?(board, index)
 end
 
 
-
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " "
+    false
+  elsif board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
